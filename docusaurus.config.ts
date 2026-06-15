c16fc589ff8425278aa4980733b285af86228491
Łukasz Zaroda <luken@luken-tech.pl>
2026-06-15 11:26:34 +0200

    Removed trailing slash.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -26,6 +26,7 @@ const config: Config = {
       },
     ],
   ],
+  trailingSlash: false,
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
